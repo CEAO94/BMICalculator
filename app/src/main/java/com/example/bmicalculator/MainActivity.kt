11ee105909f473d06d06dd36f80986452cd1b5b4
Carlos Eduardo Alves de Oliveira <94c.oliveira@gmail.com>
2025-03-01 18:18:48 +0000

    Final BMI Calc.

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -5,10 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
@@ -16,56 +13,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_main)/* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets } */
 
-        //Connect activity_main.xml with MainActivity.kt using @id
-        //Recover components EditText and Button
-        // Create VAR and UI components(EditText)
-
+// Connect the activity_main.xml layout with MainActivity.kt using @id
+// Retrieve the UI components (EditText for input and Button for action)
+// Declare variables for UI components (EditText for user input and Button for calculation)
+// Set the functionality of the Button
+// If the user input is empty, show an error message (e.g., "Please enter valid data")
+// If the input is valid, navigate to the second screen
+// Create a second screen (activity_bmi_result.xml or similar)
+// Transfer calculated BMI value from the MainActivity to the second screen ResultActivity
+// Use Intent to start the second screen (activity_bmi_result)
+// Use putExtra() to send the BMI value or other necessary data to the second screen
 
         val weight = findViewById<TextInputEditText>(R.id.inp_Weight)
         val height = findViewById<TextInputEditText>(R.id.inp_Height)
-        var result = findViewById<Button>(R.id.calc)
+        val result = findViewById<Button>(R.id.calc)
 
-        // Set Button function
         result.setOnClickListener {
             val typeWeight: String = weight.text.toString()
             val typeHeight: String = height.text.toString()
 
 
-            if (typeWeight == "" || typeHeight == "")
-            //Empty - show a error message to the user
-            {
+            if (typeWeight == "" || typeHeight == "") {
                 Snackbar.make(weight, "You must enter a valid value.", Snackbar.LENGTH_LONG).show()
-
             } else {
                 val tWeight: Float = typeWeight.toFloat() //short code line
                 val tHeight: Float = typeHeight.toString().toFloat() //long code line
 
                 val heightBMI: Float = tHeight * tHeight
-                val button = tWeight / heightBMI
-                println(button)
-
-                //Navegate to second screen;
-                //Create the second screen
-                //Transfer information between screens
-
-                // Intent - Android Class
-
+                val result = tWeight / heightBMI
 
                 val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_BMI, result)
                 startActivity(intent)
-
+                println(result)
 
             }
-
-
         }
-
-
     }
 }
--- app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -1,20 +1,41 @@
 package com.example.bmicalculator
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+const val KEY_RESULT_BMI = "ResultActivity.KEY_BMI"
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val actResult = intent.getFloatExtra(KEY_RESULT_BMI, 0f)
+
+        val numbResult = findViewById<TextView>(R.id.view_numbResult)
+        numbResult.text = actResult.toString()
+
+        val textResult = findViewById<TextView>(R.id.view_textResult)
+
+        val range: String = if (actResult <= 18.5f) {
+            "Underweight"
+        } else if (actResult > 18.5f && actResult <= 24.99f) {
+            "Normal"
+        } else if (actResult > 24.99f && actResult <= 29.99f) {
+            "Overweight"
+        } else if (actResult > 29.99f && actResult <= 34.99f) {
+            "Obesity Class I"
+        } else if (actResult > 34.99 && actResult <= 39.99) {
+            "Obesity Class II"
+        } else {
+            "Obesity Class III"
         }
+
+        textResult.text = range
     }
 }
